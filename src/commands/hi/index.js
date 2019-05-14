5ca7f8646228b24e87fe9cdea4b81ffe1b543129
Jaid <jaid.jsx@gmail.com>
2019-05-14 15:55:45 +0200

    Improved command hi
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/commands/hi/index.js
+++ src/commands/hi/index.js
@@ -1,5 +1,23 @@
+import moment from "moment"
+
 export default {
-  handle({msg, say}) {
-    say(`Hey, ${msg.userInfo.displayName}!`)
+  async handle({msg, say}) {
+    const hour = moment().hour()
+    const greeting = (() => {
+      if (hour >= 21) {
+        return "Guten Tag"
+      } else if (hour >= 18) {
+        return "Guten Abend"
+      } else if (hour >= 13) {
+        return "Guten Nachmittag"
+      } else if (hour >= 11) {
+        return "Guten Mittag"
+      } else if (hour >= 9) {
+        return "Guten Vormittag"
+      } else {
+        return "Guten Morgen"
+      }
+    })()
+    say(`${greeting}, ${msg.userInfo.displayName}!`)
   },
 }
