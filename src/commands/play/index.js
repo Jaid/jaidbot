0515d0199b15e56e2d51d69d859860ad630cf5c0
Jaid <jaid.jsx@gmail.com>
2019-05-15 00:51:44 +0200

    Improved command `play`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/commands/play/index.js
+++ src/commands/play/index.js
@@ -2,8 +2,19 @@ import execa from "execa"
 import moment from "moment"
 import momentDurationFormat from "moment-duration-format"
 import millify from "millify"
+import got from "got"
 
 momentDurationFormat(moment)
+
+const gotOptions = {
+  auth: ":1",
+  throwHttpErrors: false,
+  retry: {
+    retries: 1,
+    errorCodes: ["ETIMEDOUT", " ECONNRESET", "EADDRINUSE", "EPIPE", "ENOTFOUND", "ENETUNREACH", "EAI_AGAIN"],
+  },
+  json: true,
+}
 
 export default {
   requiredArguments: 1,
@@ -15,6 +26,22 @@ export default {
     const viewsString = millify(info.view_count, {precision: 0})
     say(`${senderDisplayName} hat "${info.title}" (${info.height}p${info.fps}, ${durationString}, ${viewsString} Klicks) von ${info.uploader} hinzugefügt!`)
     await execa("E:/Projects/node-scripts/dist/exe/playVideo.exe", [video])
-    say("Download fertig!")
+    say(info.downloadFileExists ? "Downlaod nicht nötig!" : "Download fertig!")
+    let vlcState
+    try {
+      const {body} = await got("http://127.0.0.1:8080/requests/status.json", gotOptions)
+      vlcState = body
+    } catch (error) {
+      return "Kein Lebenszeichen vom Video Player."
+    }
+    if (vlcState.state === "stopped") {
+      await got("http://127.0.0.1:8080/requests/status.json", {
+        ...gotOptions,
+        query: {
+          command: "pl_next",
+        },
+      })
+      return "Und den Video Player habe ich wieder gestartet!"
+    }
   },
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
 
 import ChatClient from "twitch-chat-client"
 import twitch from "twitch"
-import got from "got"
-import openurl2 from "openurl2"
 
 import handleMessage from "./handleMessage"
 
@@ -18,8 +16,6 @@ const streamerScopes = [
 
 const job = async () => {
   const botClient = await twitch.withCredentials(process.env.TWITCH_BOT_CLIENT_ID, process.env.TWITCH_BOT_TOKEN)
-  // const token = await twitch.getAppAccessToken(process.env.TWITCH_STREAMER_CLIENT_ID, process.env.TWITCH_STREAMER_CLIENT_SECRET)
-  // const streamerClient = await twitch.withCredentials(process.env.TWITCH_STREAMER_CLIENT_ID, token)
   const streamerClient = await twitch.withCredentials(process.env.TWITCH_STREAMER_CLIENT_ID, process.env.TWITCH_STREAMER_TOKEN, streamerScopes)
   const chatClient = await ChatClient.forTwitchClient(botClient)
   await chatClient.connect()
