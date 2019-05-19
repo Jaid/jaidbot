71f9e8044fdfe50eb32ecc0f5f32c8d7e22346a7
Jaid <jaid.jsx@gmail.com>
2019-05-19 07:58:07 +0200

    Added error message for setting volume to the current value
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/commands/volume/index.js
+++ src/commands/volume/index.js
@@ -11,6 +11,9 @@ export default {
     const currentVolume = Math.floor(vlcState.volume / 3.2)
     if (positionalArguments[0] |> isInteger) {
       const chosenVolume = Math.floor(positionalArguments[0] * 3.2)
+      if (Number(positionalArguments[0]) === currentVolume) {
+        return "Die Lautstärke wurde von ${currentVolume}% auf ${currentVolume}... Moment. Am I a joke to you?"
+      }
       await vlc.sendCommand("volume", {val: chosenVolume})
       const verb = positionalArguments[0] > currentVolume ? "angehoben" : "gesenkt"
       return `Die Lautstärke wurde von ${currentVolume}% auf ${positionalArguments[0]}% ${verb}.`
