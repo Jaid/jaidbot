e765d336c6d5e4ae0fc36dedde2be58b29ca7b8a
Jaid <jaid.jsx@gmail.com>
2019-05-28 00:00:08 +0200

    Migrated `!pause` to new architecture
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/twitch/commands/pause/index.js
+++ src/twitch/commands/pause/index.js
@@ -1,23 +1,26 @@
 import moment from "lib/moment"
-import vlc from "lib/vlc"
 import server from "src/server"
 import twitch from "src/twitch"
 
 export default {
   permission: "mod",
   needsDesktopClient: true,
-  async handle() {
-    server.client.emit("getVlcState", state => {
-      twitch.say("State")
+  async handle({sender}) {
+    server.client.emit("getVlcState", vlcState => {
+      if (!vlcState) {
+        twitch.say(`Kein Lebenszeichen aus dem Kino. Sorry, ${sender.displayName}!`)
+        return
+      }
+      const durationString = moment.duration(vlcState.time, "seconds").format()
+      const command = vlcState.state === "playing" ? "pl_forcepause" : "pl_play"
+      const answer = vlcState.state === "playing" ? `Pausiert bei ${durationString}, Bruder! Jetzt hast du deine Ruhe.` : `Geht heiter weiter an der Stelle ${durationString}!`
+      server.client.emit("sendVlcCommand", command, null, result => {
+        if (result) {
+          twitch.say(answer)
+        } else {
+          twitch.say("Da hat etwas nicht geklappt")
+        }
+      })
     })
-    // const vlcState = await vlc.getState()
-    // if (!vlcState) {
-    //   return "Kein Lebenszeichen vom Video Player."
-    // }
-    // const durationString = moment.duration(vlcState.time, "seconds").format()
-    // const command = vlcState.state === "playing" ? "pl_forcepause" : "pl_play"
-    // const answer = vlcState.state === "playing" ? `Pausiert bei ${durationString}, Bruder! Jetzt hast du deine Ruhe.` : `Geht heiter weiter an der Stelle ${durationString}!`
-    // await vlc.sendCommand(command)
-    // return answer
   },
 }
