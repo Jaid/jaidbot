66f3328aab9cee224742fcfe21d9355e1948f695
Jaid <jaid.jsx@gmail.com>
2019-06-06 00:53:25 +0200

    Added mod permission to `!title`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/twitch/commands/title/index.js
+++ src/twitch/commands/title/index.js
@@ -1,6 +1,7 @@
 import afkManager from "src/twitch/afkManager"
 
 export default {
+  permission: "mod",
   async handle({combinedArguments: newTitle}) {
     await afkManager.setTitle(newTitle)
     return `Dieser Stream wurde umgetauft zu "${newTitle}". Amen.`
