7834b4444354e9b00eb79ec8d1b7a1f8c8f4abde
Jaid <jaid.jsx@gmail.com>
2019-06-17 22:27:37 +0200

    Migrated to `essential-config` 3.0.0
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/config/index.js
+++ src/lib/config/index.js
@@ -1,16 +1,32 @@
 import essentialConfig from "essential-config"
 import logger from "lib/logger"
-import appdataPath from "appdata-path"
 
-import defaultConfig from "./defaults.yml"
+import defaults from "./defaults.yml"
 
-const config = essentialConfig(_PKG_TITLE, defaultConfig)
+const config = essentialConfig(_PKG_TITLE, {
+  defaults,
+  sensitiveKeys: [
+    "serverPassword",
+    "twitchBotClientId",
+    "twitchBotClientToken",
+    "twitchApiClientId",
+    "twitchApiClientToken",
+    "travisToken",
+    "youtubeClientId",
+    "youtubeClientSecret",
+    "youtubeClientAccessToken",
+    "youtubeClientRefreshToken",
+    "twitterConsumerKey",
+    "twitterConsumerSecret",
+    "twitterAccessToken",
+    "twitterAccessSecret",
+  ],
+})
 
 if (!config) {
   logger.warn("Set up default config, please edit and restart")
   process.exit(2)
 }
 
-config.appFolder = appdataPath(_PKG_TITLE)
-
-export default config
+export const appFolder = config.appFolder
+export default config.config
