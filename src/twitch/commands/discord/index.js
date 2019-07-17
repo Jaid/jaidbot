a31a86de790581a0e545871e9dddf9206168e687
Jaid <jaid.jsx@gmail.com>
2019-07-17 12:13:32 +0200

    Fixed command `!discord`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/twitch/commands/discord/index.js
+++ src/twitch/commands/discord/index.js
@@ -1,5 +1,5 @@
 export default {
-  handle() {
-    return "Hier ist ein Discord Server: discordapp.com/invite/Mnk8QuG"
+  async handle() {
+    return "Discord Server: discordapp.com/invite/Mnk8QuG"
   },
 }
