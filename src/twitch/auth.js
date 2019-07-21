28cb8138dfd22104cab237088a08658d0003be46
Jaid <jaid.jsx@gmail.com>
2019-07-21 21:46:45 +0200

    feat: Implemented Twitch auth
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/twitch/auth.js
+++ src/twitch/auth.js
@@ -1,7 +1,10 @@
 import express from "express"
 import {Passport} from "passport"
-import {Strategy as TwitchStrategy} from "passport-twitch"
+import {Strategy as TwitchStrategy} from "passport-twitch-new"
 import config from "lib/config"
+import logger from "lib/logger"
+
+import indexContent from "!raw-loader!./index.html"
 
 class Auth {
 
@@ -24,8 +27,20 @@ class Auth {
         "channel_read",
       ],
     }, (accessToken, refreshToken, profile, done) => {
+      logger.info("A")
       debugger
     }))
+
+    this.app.get("/", (request, response) => {
+      response.send(indexContent)
+    })
+    this.app.get("/auth/twitch", this.passport.authenticate("twitch"))
+    this.app.get("/auth/twitch/callback", this.passport.authenticate("twitch", {failureRedirect: "/"}), (request, response) => {
+      response.send("OK")
+    })
+
+    this.app.listen(config.twitchAuthPort)
+    logger.info("Starting Twitch auth server on port %o", config.twitchAuthPort)
   }
 
 }
