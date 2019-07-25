a5284d1cd6770537ecd141ae61f274c3a0830c00
Jaid <jaid.jsx@gmail.com>
2019-07-25 14:52:25 +0200

    improve: Improved stability of YouTube subscription watcher
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/youtube/subscriptionWatcher.js
+++ src/youtube/subscriptionWatcher.js
@@ -7,6 +7,9 @@ import {flatten} from "lodash"
 import ensureObject from "ensure-object"
 import Video from "src/models/Video"
 import regexParser from "regex-parser"
+import pMinDelay from "p-min-delay"
+import ms from "ms.macro"
+import pAll from "p-all"
 
 /**
  * @typedef {Object} YoutubeVideo
@@ -26,21 +29,25 @@ class SubscriptionWatcher extends PollingEmitter {
   constructor() {
     super({
       pollInterval: config.youtubeSubscriptionsPollIntervalSeconds * 1000,
-      invalidateInitialEntries: true,
+      invalidateInitialEntries: false,
     })
     this.startDate = Date.now()
     this.on("newEntry", /** @type {newEntryHandler} */ async youtubeVideo => {
-      if (youtubeVideo.channel.titleRegex) {
-        if (!regexParser(youtubeVideo.channel.titleRegex).test(youtubeVideo.title)) {
-          logger.info("Skipping new video \"%s\" by %s, because it failed RegExp test %s", youtubeVideo.title, youtubeVideo.channel.name, youtubeVideo.channel.titleRegex)
-          return
+      try {
+        if (youtubeVideo.channel?.titleRegex) {
+          if (!regexParser(youtubeVideo.channel.titleRegex).test(youtubeVideo.title)) {
+            logger.info("Skipping new video \"%s\" by %s, because it failed RegExp test %s", youtubeVideo.title, youtubeVideo.channel.name, youtubeVideo.channel.titleRegex)
+            return
+          }
         }
+        const url = `youtu.be/${youtubeVideo.id}`
+        const video = await Video.queueByUrl(url, {
+          priority: youtubeVideo.channel.priority || config.videoSubscriptionPriority,
+        })
+        twitch.say(`PopCorn Video "${video.title}" von ${youtubeVideo.channel.name || video.publisher}: ${url}`)
+      } catch (error) {
+        logger.error("Found new YouTube video %s, but couldn't process it: %s", youtubeVideo.id, error)
       }
-      const url = `youtu.be/${youtubeVideo.id}`
-      const video = await Video.queueByUrl(url, {
-        priority: youtubeVideo.channel.priority || config.videoSubscriptionPriority,
-      })
-      twitch.say(`PopCorn Video "${video.title}" von ${youtubeVideo.channel.name || video.publisher}: ${url}`)
     })
   }
 
@@ -50,20 +57,25 @@ class SubscriptionWatcher extends PollingEmitter {
   }
 
   async fetchEntries() {
-    const fetchJobs = config.observedYoutubeChannels.map(async entry => {
+    const fetchJobs = [config.observedYoutubeChannels].map(async entry => {
       /**
        * @type {import("../lib/config").ObservedYoutubeChannel}
        */
-      const youtubeChannel = ensureObject(entry, "id")
+      const channel = ensureObject(entry, "id")
       /**
        * @type {YoutubeVideo}
        */
-      const youtubeVideo = await fetchYoutubeUploads(youtubeChannel.id)
-      youtubeVideo.channel = youtubeChannel
-      return youtubeVideo
+      logger.debug("Fetching YouTube videos from %s", channel.name)
+      const youtubeVideos = await pMinDelay(fetchYoutubeUploads(channel.id), ms`1 minute`, {
+        delayRejection: false,
+      })
+      return youtubeVideos.map(video => ({
+        ...video,
+        channel,
+      }))
     })
     logger.debug("Fetching videos from %s YouTube channels", fetchJobs.length)
-    const results = await Promise.all(fetchJobs)
+    const results = await pAll(fetchJobs, {concurrency: 3})
     const resultsList = flatten(results)
     logger.debug("Fetched %s videos from %s YouTube channels", resultsList.length, fetchJobs.length)
     return resultsList
