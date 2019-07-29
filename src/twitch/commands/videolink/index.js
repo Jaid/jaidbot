83e5e4f956eeef5cb2f541eb11fa7ceffef65e71
Jaid <jaid.jsx@gmail.com>
2019-07-29 17:39:19 +0200

    improve: Improved output of `!videolink`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/twitch/commands/videolink/index.js
+++ src/twitch/commands/videolink/index.js
@@ -2,15 +2,19 @@ import Video from "src/models/Video"
 
 export default {
   needsDesktopClient: true,
-  async handle() {
+  async handle({sender}) {
     const video = await Video.getCurrentVideo()
     if (!video) {
-      return
+      return `Es gibt gerade kein abgespieltes Video, ${sender.displayName}.`
     }
     let url
     if (video.extractor === "youtube") {
-      const timestampSeconds = Math.floor(video.timestamp / 1000)
-      url = `https://youtu.be/${video.mediaId}?t=${timestampSeconds}`
+      let query = ""
+      if (video.timestamp > 0 && !video.hasBeenWatched()) {
+        const timestampSeconds = Math.floor(video.timestamp / 1000)
+        query = `?t=${timestampSeconds}`
+      }
+      url = `https://youtu.be/${video.mediaId}${query}`
     } else {
       url = video.webUrl
     }
