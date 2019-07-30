447ed0ea56455a464d5409091e80bd5cc272ca54
Jaid <jaid.jsx@gmail.com>
2019-07-30 03:12:01 +0200

    feat: Added command `!queuesize`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ src/twitch/commands/queuesize/index.js
@@ -0,0 +1,28 @@
+import Video from "src/models/Video"
+import {Op} from "sequelize"
+import humanizeDuration from "lib/humanizeDuration"
+
+export default {
+  async handle() {
+    const data = await Video.findAll({
+      where: {
+        watchedAt: {
+          [Op.eq]: null,
+        },
+      },
+      raw: true,
+      attributes: [
+        "timestamp",
+        "duration",
+        "vlcDuration",
+      ],
+    })
+    const durationSum = data.reduce((sum, current) => {
+      const duration = current.vlcDuration || current.duration
+      const timestamp = current.timestamp || 0
+      const actualDuration = duration - timestamp
+      return sum + actualDuration
+    }, 0)
+    return `Es sind ${data.length} ungesehene Videos in der Liste. Da ist noch eine Laufzeit von ${durationSum |> humanizeDuration}.`
+  },
+}
