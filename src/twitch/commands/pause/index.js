bcc2464b5ae201d92155f983a553bb3df60e543d
Jaid <jaid.jsx@gmail.com>
2019-08-02 01:46:51 +0200

    improve: Improved output of `!pause`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/twitch/commands/pause/index.js
+++ src/twitch/commands/pause/index.js
@@ -14,7 +14,7 @@ export default {
     const result = await Video.sendVlcCommand(command)
     if (result) {
       const durationString = moment.duration(vlcState.time, "seconds").format()
-      const answer = vlcState.state === "playing" ? `Pausiert bei ${durationString}, Bruder! Jetzt hast du deine Ruhe.` : `Geht heiter weiter an der Stelle ${durationString}!`
+      const answer = vlcState.state === "playing" ? `PopCorn Pausiert bei ${durationString}, Bruder! Jetzt hast du deine Ruhe.` : `PopCorn Geht heiter weiter an der Stelle ${durationString}!`
       twitch.say(answer)
     } else {
       twitch.say("Da hat etwas nicht geklappt")
