a9423a6eba859b4f8038e87866986c99b48dbf40
Jaid <jaid.jsx@gmail.com>
2019-09-26 13:31:58 +0200

    improve: `!adventure` can now be used by all chatters
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/twitch/commands/adventure/index.js
+++ src/twitch/commands/adventure/index.js
@@ -2,14 +2,13 @@ import twitch from "src/twitch"
 import ms from "ms.macro"
 
 export default {
-  permission: "mod",
-  async handle() {
+  async handle({sender}) {
     setTimeout(() => {
-      twitch.say("Der zweite Würfelwurf in Black Spirit's Adventure ist bereit!")
+      twitch.say(`${sender.displayName}, der zweite Würfelwurf in Black Spirit's Adventure ist bereit!`)
     }, ms`61 minutes`)
     setTimeout(() => {
-      twitch.say("Der letzte Würfelwurf in Black Spirit's Adventure ist bereit!")
+      twitch.say(`${sender.displayName}, Der letzte Würfelwurf in Black Spirit's Adventure ist bereit!`)
     }, ms`122 minutes`)
-    return "Hoffentlich gab es keinen blöden Buff!"
+    return `Hoffentlich gab es keinen blöden Buff, ${sender.displayName}!`
   },
 }
