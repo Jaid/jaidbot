341b1b648b85c9edea6284204607529c2dfa7ffb
Jaid <jaid.jsx@gmail.com>
2019-09-27 14:26:47 +0200

    fix: Fixed typo in `!adventure` output
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/twitch/commands/adventure/index.js
+++ src/twitch/commands/adventure/index.js
@@ -7,7 +7,7 @@ export default {
       twitch.say(`${sender.displayName}, der zweite Würfelwurf in Black Spirit's Adventure ist bereit!`)
     }, ms`61 minutes`)
     setTimeout(() => {
-      twitch.say(`${sender.displayName}, Der letzte Würfelwurf in Black Spirit's Adventure ist bereit!`)
+      twitch.say(`${sender.displayName}, der letzte Würfelwurf in Black Spirit's Adventure ist bereit!`)
     }, ms`122 minutes`)
     return `Hoffentlich gab es keinen blöden Buff, ${sender.displayName}!`
   },
