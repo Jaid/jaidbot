2f94a4240246a4e2b737a5988e1fce9bdd8c7742
Jaid <jaid.jsx@gmail.com>
2019-10-08 20:28:55 +0200

    fix: Fixed twitter tweet check logic
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/plugins/twitter/index.js
+++ src/plugins/twitter/index.js
@@ -5,19 +5,6 @@ import {unpackObject} from "magina"
 import zahl from "zahl"
 import {isEmpty} from "has-content"
 
-const isOwnTweet = tweet => {
-  if (tweet.retweeted_status) {
-    return false
-  }
-  if (tweet.in_reply_to_user_id && tweet.in_reply_to_user_id !== tweet.user.id) {
-    return false
-  }
-  if (!tweet?.user?.id_str) {
-    return false
-  }
-  return this.followedIds.includes(tweet.user.id_str)
-}
-
 const getTweetText = tweet => {
   return tweet.extended_tweet?.full_text || tweet.text
 }
@@ -50,18 +37,31 @@ export default class TweetWatcher {
       filter_level: "none",
     })
     tweetEmitter.on("tweet", tweet => {
-      if (!isOwnTweet(tweet)) {
+      if (!this.isOwnTweet(tweet)) {
         return
       }
       twitch.say(`MercyWing2 ${tweet.user.name} tweetet: ${tweet |> getTweetText}`)
     })
     tweetEmitter.on("delete", tweet => {
-      if (!isOwnTweet(tweet)) {
+      if (!this.isOwnTweet(tweet)) {
         return
       }
       twitch.say(`CurseLit ${tweet.user.name} hat einen Tweet gelöscht: ${tweet.text}`)
     })
     logger.info("Started tweet notifier for %s", zahl(this.followedIds, "profile"))
+  }
+
+  isOwnTweet = tweet => {
+    if (tweet.retweeted_status) {
+      return false
+    }
+    if (tweet.in_reply_to_user_id && tweet.in_reply_to_user_id !== tweet.user.id) {
+      return false
+    }
+    if (!tweet?.user?.id_str) {
+      return false
+    }
+    return this.followedIds.includes(tweet.user.id_str)
   }
 
 }
