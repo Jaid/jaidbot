5521b7eab2d15222bfb00f702fa0540e3e56676c
Jaid <jaid.jsx@gmail.com>
2019-10-25 12:00:17 +0200

    improve: Improved output of `!skip`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/twitch/commands/skip/index.js
+++ src/twitch/commands/skip/index.js
@@ -1,4 +1,13 @@
 import Video from "src/models/Video"
+import humanizeDuration from "humanize-duration"
+import ms from "ms.macro"
+
+const humanizeDurationShort = humanizeDuration.humanizer({
+  language: "de",
+  conjunction: " und ",
+  serialComma: false,
+  maxDecimalPoints: 0,
+})
 
 export default {
   permission: "mod",
@@ -13,8 +22,15 @@ export default {
       watchedAt: new Date,
     })
     const result = await Video.sendVlcCommand("pl_stop")
-    if (result) {
+    if (!result) {
+      return
+    }
+    const durationMs = video.getDurationMs()
+    const currentTime = video.timestamp
+    if (currentTime / durationMs > 0.5) {
       return `Skippie! Weg mit "${video.title}"!`
     }
+    const skippedMs = Math.max(ms`2 seconds`, durationMs - currentTime)
+    return `Alles klar, dann schau dir halt nicht die letzten ${humanizeDurationShort(skippedMs)} von "${video.title}" an.`
   },
 }
