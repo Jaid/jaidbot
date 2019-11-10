a86dce9543bfc77c6e33892eed6b9db017987994
Jaid <jaid.jsx@gmail.com>
2019-11-10 21:12:48 +0100

    improve: Changed timings for `!remindworker`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/twitch/commands/remindworker/index.js
+++ src/twitch/commands/remindworker/index.js
@@ -4,7 +4,7 @@ import ScheduledMessage from "src/models/ScheduledMessage"
 export default {
   async handle({senderName}) {
     await Promise.all([
-      ScheduledMessage.addForDelay(ms`9 minutes`, `${senderName}, in einer Minute kommt der Worker!`),
+      ScheduledMessage.addForDelay(ms`560 seconds`, `${senderName}, in 40 Sekunden kommt der Worker!`),
       ScheduledMessage.addForDelay(ms`595 seconds`, `${senderName}, der Worker ist da!`),
     ])
     return `In 10 Minuten lässt sich der Worker beim Supervisor erwerben. Ich sage noch mal rechtzeitig Bescheid, ${senderName}!`
