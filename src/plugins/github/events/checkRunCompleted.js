b588f8fde40ea274e23e1ffc11c87a4ced07dfea
Jaid <jaid.jsx@gmail.com>
2019-12-06 23:57:44 +0100

    feat: Added Twitch chat feedback for every completed GitHub check run (Could be pretty spammy)
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/plugins/github/events/checkRunCompleted.js
+++ src/plugins/github/events/checkRunCompleted.js
@@ -1,5 +1,3 @@
-import {isEmpty} from "has-content"
-
 import twitch from "src/plugins/twitch"
 
 export default async event => {
@@ -7,10 +5,25 @@ export default async event => {
    * @type {import("@octokit/webhooks").WebhookPayloadCheckRun}
    */
   const payload = event.payload
+  /**
+   * @type {import("@octokit/rest").Octokit}
+   */
+  const github = event.github
   if (payload.repository.private) {
     return
   }
+  // const checkRun = await github.checks.get({
+  //   owner: payload.repository.owner.login,
+  //   repo: payload.repository.name,
+  //   check_run_id: payload.check_run.id,
+  // })
+  // const checkSuite = await github.checks.getSuite({
+  //   owner: payload.repository.owner.login,
+  //   repo: payload.repository.name,
+  //   check_suite_id: payload.check_run.check_suite.id,
+  // })
   if (payload.check_run.conclusion === "success") {
+    twitch.say(`Action für ${payload.repository.name} wurde erfolgreich abgeschlossen!`) // TODO: Remove soon, too spammy
     return
   }
   twitch.say(`Action für ${payload.repository.name} ist fehlgeschlagen (${payload.check_run.conclusion}): ${payload.check_run.html_url}`)
