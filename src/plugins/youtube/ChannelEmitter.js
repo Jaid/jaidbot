6fd06a17f99e571baf360915036affbf1abef86a
Jaid <jaid.jsx@gmail.com>
2020-02-14 23:41:03 +0100

    Redone YouTube checks
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ src/plugins/youtube/ChannelEmitter.js
@@ -0,0 +1,38 @@
+import fetchYoutubeUploads from "fetch-youtube-uploads"
+import {isEmpty} from "has-content"
+import PollingEmitter from "polling-emitter"
+
+export default class ChannelEmitter extends PollingEmitter {
+
+  constructor(channel, logger, pollInterval) {
+    super({
+      autostart: true,
+      pollInterval,
+    })
+    this.channel = channel
+    this.logger = logger
+  }
+
+  async fetchEntries() {
+    const results = []
+    const youtubeVideos = await fetchYoutubeUploads(this.channel.id)
+    if (isEmpty(youtubeVideos)) {
+      throw new Error(`Fetched no videos from ${this.channel.name || this.channel.id}`)
+    }
+    for (const video of youtubeVideos) {
+      if (!video.published) {
+        continue
+      }
+      results.push({
+        ...video,
+        channel: this.channel,
+      })
+    }
+    return results
+  }
+
+  handleError(error) {
+    this.logger.warn(error)
+  }
+
+}
--- src/plugins/youtube/index.js
+++ src/plugins/youtube/index.js
@@ -1,16 +1,15 @@
 import delay from "delay"
 import ensureObject from "ensure-object"
-import fetchYoutubeUploads from "fetch-youtube-uploads"
 import {isEmpty} from "has-content"
-import pMap from "p-map"
-import pMinDelay from "p-min-delay"
-import PollingEmitter from "polling-emitter"
+import {JaidCorePlugin} from "jaid-core"
 import regexParser from "regex-parser"
 import zahl from "zahl"
 
 import {logger} from "src/core"
 import Video from "src/models/Video"
 import twitch from "src/plugins/twitch"
+
+import ChannelEmitter from "./ChannelEmitter"
 
 /**
  * @typedef {Object} YoutubeVideo
@@ -25,95 +24,69 @@ import twitch from "src/plugins/twitch"
  * @param {YoutubeVideo} youtubeVideo
  */
 
-export default class SubscriptionWatcher extends PollingEmitter {
-
-  constructor() {
-    super({
-      invalidateInitialEntries: true,
-    })
-  }
+export default class SubscriptionWatcher extends JaidCorePlugin {
 
   handleConfig(config) {
     this.observedChannels = config.observedYoutubeChannels
-    this.options.pollInterval = config.youtubeSubscriptionsPollIntervalSeconds * 1000
+    this.pollInterval = config.youtubeSubscriptionsPollIntervalSeconds * 1000
     this.defaultPriority = config.videoSubscriptionPriority
     this.forcedTimeBetweenChecks = config.secondsBetweenYoutubeChecks * 1000
+  }
+
+  /**
+   * @param {YoutubeVideo} youtubeVideo
+   * @return {Promise<void>}
+   */
+  async handleNewVideo(youtubeVideo) {
+    try {
+      if (youtubeVideo.channel?.titleRegex && !regexParser(youtubeVideo.channel.titleRegex).test(youtubeVideo.title)) {
+        logger.info("Skipping new video \"%s\" by %s, because it failed RegExp test %s", youtubeVideo.title, youtubeVideo.channel.name, youtubeVideo.channel.titleRegex)
+        return
+      }
+      if (youtubeVideo.channel?.titleRegexNot && regexParser(youtubeVideo.channel.titleRegexNot).test(youtubeVideo.title)) {
+        logger.info("Skipping new video \"%s\" by %s, because it succeeded RegExp test %s", youtubeVideo.title, youtubeVideo.channel.name, youtubeVideo.channel.titleRegexNot)
+        return
+      }
+      const url = `youtu.be/${youtubeVideo.id}`
+      if (youtubeVideo.channel.name) {
+        twitch.say(`PopCorn Video "${youtubeVideo.title}" von ${youtubeVideo.channel.name}: ${url}`)
+      } else {
+        twitch.say(`PopCorn Video "${youtubeVideo.title}": ${url}`)
+      }
+      await Video.queueByUrl(url, {
+        priority: youtubeVideo.channel.priority || this.defaultPriority,
+      })
+    } catch (error) {
+      logger.error("Found new YouTube video %s, but couldn't process it: %s", youtubeVideo.id, error)
+    }
   }
 
   init() {
     if (isEmpty(this.observedChannels)) {
       return false
     }
-    this.on("initialEntry", /** @type {newEntryHandler} */ youtubeVideo => {
-      logger.debug("Found video \"%s\", but it is not new.", youtubeVideo.title)
-    })
-    this.on("newEntry", /** @type {newEntryHandler} */ async youtubeVideo => {
-      try {
-        if (youtubeVideo.channel?.titleRegex && !regexParser(youtubeVideo.channel.titleRegex).test(youtubeVideo.title)) {
-          logger.info("Skipping new video \"%s\" by %s, because it failed RegExp test %s", youtubeVideo.title, youtubeVideo.channel.name, youtubeVideo.channel.titleRegex)
-          return
-        }
-        if (youtubeVideo.channel?.titleRegexNot && regexParser(youtubeVideo.channel.titleRegexNot).test(youtubeVideo.title)) {
-          logger.info("Skipping new video \"%s\" by %s, because it succeeded RegExp test %s", youtubeVideo.title, youtubeVideo.channel.name, youtubeVideo.channel.titleRegexNot)
-          return
-        }
-        const url = `youtu.be/${youtubeVideo.id}`
-        if (youtubeVideo.channel.name) {
-          twitch.say(`PopCorn Video "${youtubeVideo.title}" von ${youtubeVideo.channel.name}: ${url}`)
-        } else {
-          twitch.say(`PopCorn Video "${youtubeVideo.title}": ${url}`)
-        }
-        await Video.queueByUrl(url, {
-          priority: youtubeVideo.channel.priority || this.defaultPriority,
-        })
-      } catch (error) {
-        logger.error("Found new YouTube video %s, but couldn't process it: %s", youtubeVideo.id, error)
+    this.collectPollingEmitters()
+  }
+
+  async collectPollingEmitters() {
+    const handleNewVideo = this.handleNewVideo.bind(this)
+    this.pollingEmitters = []
+    let index = 0
+    for (const observedChannel of this.observedChannels) {
+      if (index !== 0 && this.forcedTimeBetweenChecks > 0) {
+        await delay(this.forcedTimeBetweenChecks)
       }
-    })
+      const channel = ensureObject(observedChannel, "id")
+      const channelEmitter = new ChannelEmitter(channel, logger, this.pollInterval)
+      channelEmitter.on("newEntry", handleNewVideo)
+      this.pollingEmitters.push(channelEmitter)
+      index++
+    }
+    logger.info("Started YouTube subscriptionWatcher for %s", zahl(this.pollingEmitters, "subscription"))
   }
 
   postInit() {
     return twitch.isReady
-  }
-
-  ready() {
-    this.start()
-    logger.info("Started YouTube subscriptionWatcher for %s", zahl(this.observedChannels, "subscription"))
-  }
-
-  async fetchEntries() {
-    const results = []
-    const mapper = async entry => {
-      /**
-       * @type {import("../lib/config").ObservedYoutubeChannel}
-       */
-      const channel = ensureObject(entry, "id")
-      logger.debug("Fetching YouTube videos from %s", channel.name)
-      try {
-        const youtubeVideos = await pMinDelay(fetchYoutubeUploads(channel.id), this.forcedTimeBetweenChecks, {
-          delayRejection: false,
-        })
-        for (const video of youtubeVideos) {
-          if (!video.published) {
-            continue
-          }
-          results.push({
-            ...video,
-            channel,
-          })
-        }
-      } catch (error) {
-        logger.error("Could not fetch YouTube channel %s", channel.name)
-        throw error
-      }
-    }
-    await pMap(this.observedChannels, mapper, {concurrency: 1})
-    logger.debug("Fetched %s from %s", zahl(results, "video"), zahl(this.observedChannels, "channel"))
-    return results
-  }
-
-  handleError(error) {
-    logger.error("Could not check YouTube subscriptions: %s", error)
   }
 
 }
